d5de314021c4d19aedec626d3f1abc34de5ed49a
Rolf Degen <rolfdegen@hotmail.com>
2022-02-28 11:40:43 +0100

    Add files via upload
    Firmware Update V1.37

---
+++ Jeannie/Oscilloscope.h
@@ -0,0 +1,92 @@
+//*************************************************************************
+// ElectroTechnique TSynth scope
+// Change scale for TFT ER-TFTM018-3 Display 128x160 Pixel
+// and implement Idle flag when no audio signal 
+//
+// https://github.com/ElectroTechnique/TSynth-Teensy4.1
+//
+//*************************************************************************
+#ifndef Oscilloscope_h_
+#define Oscilloscope_h_
+#include "AudioStream.h"
+#include "ST7735_t3.h"
+boolean EnvIdelFlag = false;
+boolean scopeIdel = false;
+
+uint8_t bufferBlock = 0;
+uint8_t bufcount = 0;
+uint8_t pixel_x = 0;
+int16_t pixel_y = 0;
+int16_t prev_pixel_y = 0;
+
+class Oscilloscope : public AudioStream {
+	public:
+	Oscilloscope(void) : AudioStream(1, inputQueueArray) {
+	}
+	virtual void update(void);
+	void ScreenSetup(ST7735_t3*);
+	void Display(void);
+	void AddtoBuffer(int16_t*);
+
+	private:
+	audio_block_t *inputQueueArray[1];
+	ST7735_t3 *display;
+	int16_t buffer[AUDIO_BLOCK_SAMPLES];
+
+};
+#endif
+
+void Oscilloscope::ScreenSetup(ST7735_t3 *screen) {
+	display = screen;
+}
+
+void Oscilloscope::Display() {
+	pixel_x = 0;
+	prev_pixel_y = map(buffer[0], 32767, -32768, -120, 120) + 95;
+	if (prev_pixel_y < 65) prev_pixel_y = 65;
+	if (prev_pixel_y > 126)prev_pixel_y = 126;
+
+	for (uint8_t i = 0; i < AUDIO_BLOCK_SAMPLES - 1; i++) {
+		pixel_y = map(buffer[i], 32767, -32768, -120, 120) + 95;
+		if (pixel_y < 65) pixel_y = 65;
+		if (pixel_y > 126)pixel_y = 126;
+		display->drawLine(pixel_x + 15, prev_pixel_y, pixel_x + 16, pixel_y, 0x07B0);
+		prev_pixel_y = pixel_y;
+		pixel_x++;
+	}
+}
+
+void Oscilloscope::AddtoBuffer(int16_t *audio) {
+	audio++;
+	if (bufferBlock == 0) {
+		if (*(audio - 1) > -16 && *(audio + 3) < 16) {
+			bufferBlock = 1;
+			bufcount = 0;
+		}
+	}
+	else {
+		for (uint16_t i = 0; i < 32; i++) {
+			buffer[bufcount++] = *audio;
+			audio += 4;
+		}
+		bufferBlock++;
+		if (bufferBlock >= 5) {
+			bufferBlock = 0;
+		}
+	}
+}
+
+void Oscilloscope::update(void) {
+	if (!display || scopeIdel == true) return;
+	audio_block_t *block;
+	block = receiveReadOnly(0);
+	if (block) {
+		AddtoBuffer(block->data);
+		release(block);
+		if (bufferBlock == 0) {
+			if (EnvIdelFlag == true) {
+				Display();
+			}
+		}
+	}
+}
