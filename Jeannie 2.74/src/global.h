6f9016a1ec2879c47ee420864ffcf9b51fe22556
Rolf Degen <rolfdegen@hotmail.com>
2023-09-27 18:27:23 +0200

    Add Mutable Braids oscillator and PlatformIO source
    Add Mutable Braids oscillator and PlatformIO source

---
+++ Jeannie 2.74/src/global.h
@@ -0,0 +1,44 @@
+// global.h
+#ifndef __global_h__
+#define __global_h__
+
+extern int PageNr;
+extern int LastPageNr;
+extern int PageNr_old;
+extern int Number_of_Pages;
+extern boolean tftUpdate;
+extern boolean ParUpdate;
+extern int ADC_Ch1;
+extern int ADC_Ch2;
+extern int ADC_Ch3;
+extern int ADC_Ch4;
+extern int ADC_Ch5;
+extern int ADC_Ch6;
+extern int ADC_Ch7;
+extern int ADC_Ch8;
+extern int WaveNr;
+extern uint8_t Osc1WaveBank;
+extern uint8_t Osc2WaveBank;
+extern uint8_t FilterTyp;
+extern uint8_t FilterCut_value;
+extern uint8_t FilterRes_value;
+extern uint8_t MidiStatusSymbol;
+extern uint16_t MidiStatusHoldTime;
+extern uint8_t FxPrgNo;
+extern uint8_t FxMixValue;
+extern uint8_t FxTimeValue;
+extern uint8_t FxFeedbackValue;
+extern uint8_t FxPot1value;
+extern uint8_t FxPot2value;
+extern uint8_t FxPot3value;
+extern uint8_t myFxSelValue;
+extern uint8_t myFxValValue;
+extern uint8_t currentPatchNameStatus;
+//extern int currentPatchBank;
+//extern int patchNo;
+extern uint8_t myLFO1shapeNo;
+extern uint8_t myLFO2shapeNo;
+extern boolean MainShift;
+
+#endif
+
